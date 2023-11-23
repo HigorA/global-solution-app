d40c7832677e9548f076d4cce658fbd55f52c881
Lucas <lumarques.souza03@gmail.com>
2023-11-23 05:43:42 -0300

    Images and adding button.

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,23 +1,44 @@
-import { SafeAreaView, Text, StyleSheet, Image} from "react-native";
+import { SafeAreaView, Text, StyleSheet, Image, Button, View } from 'react-native';
 
 export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.containerSafe}>
 
-    return(
-        <SafeAreaView style={styles.conteinerSafe}>
-            <Text>
-                Home Screen
-            </Text>
-            <Image
-                source={require('./../../../assets/mae.png')}
-                resizeMode="cover"
-                style={{width: 200, height: 200, alignSelf: 'center'}}
-            />
-        </SafeAreaView>
-    )
+
+      <Image
+        source={require('./../../../assets/logo.png')}
+        resizeMode="cover"
+        style={{ width: 200, height: 200, alignSelf: 'center', marginBottom: 250 }}
+      />
+
+      <Image
+        source={require('./../../../assets/mae.png')}
+        resizeMode="cover"
+        style={{ width: 200, height: 200, alignSelf: 'center' }}
+      />
+    
+        
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Entrar"
+          color="#f58581"
+          onPress={() => {}}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    conteinerSafe:{
-        backgroundColor: '#FFCAB3'
-    }
-})
+  containerSafe: {
+    backgroundColor: '#FFCAB3',
+    flex: 1, 
+  },
+  buttonContainer: {
+    width: '80%', 
+    borderRadius: 20, 
+    overflow: 'hidden', 
+    alignSelf: 'center',
+    marginTop: 250 
+  },
+});
