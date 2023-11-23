0c47616124f52a6934b17a91b9d6c348e07d9b4c
Lucas <lumarques.souza03@gmail.com>
2023-11-23 03:02:52 -0300

    Primeiro commit

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,12 +1,21 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, StyleSheet, Image} from "react-native";
 
 export default function HomeScreen() {
 
     return(
-        <SafeAreaView>
+        <SafeAreaView style={styles.conteinerSafe}>
             <Text>
                 Home Screen
             </Text>
+            <Image
+            source={require('')}
+            />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    conteinerSafe:{
+        backgroundColor: '#F78881'
+    }
+})
