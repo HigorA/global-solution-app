72727b397bea3f4a221fba86514221f5ba1f7967
Lucas <lumarques.souza03@gmail.com>
2023-11-23 11:01:42 -0300

    TipsScreen Update

--- src/MainNavigator.js
+++ src/MainNavigator.js
@@ -10,7 +10,7 @@ export default function MainNavigator() {
     const Stack = createNativeStackNavigator();
 
     return(
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="Tips" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Home" component={HomeScreen} /> 
             <Stack.Screen name="Login" component={LoginScreen} /> 
             <Stack.Screen name="Register" component={RegisterScreen} /> 
