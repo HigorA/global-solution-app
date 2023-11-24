78b928fc034a773dc64550bd51c82c514593dec9
Higor <higor.a.santos12@hotmail.com>
2023-11-24 04:28:37 -0300

    Menu Screen

--- src/MainNavigator.js
+++ src/MainNavigator.js
@@ -3,6 +3,7 @@ import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import TipsScreen from "./screens/TipsScreen";
+import MenuScreen from "./screens/MenuScreen";
 
 
 export default function MainNavigator() {
@@ -10,11 +11,12 @@ export default function MainNavigator() {
     const Stack = createNativeStackNavigator();
 
     return(
-        <Stack.Navigator initialRouteName="Tips" screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="Menu" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Home" component={HomeScreen} /> 
             <Stack.Screen name="Login" component={LoginScreen} /> 
             <Stack.Screen name="Register" component={RegisterScreen} /> 
             <Stack.Screen name="Tips" component={TipsScreen} />
+            <Stack.Screen name="Menu" component={MenuScreen} />
         </Stack.Navigator>
     )
 }
